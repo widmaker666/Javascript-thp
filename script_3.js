2d93c08887b7699cc0ca603b1bbdbc9b93cd8eae
widmaker666 <alexandre.hontcharouk69@gmail.com>
2022-11-17 18:17:35 +0100

    fini le script 3

---
+++ script_3.js
@@ -0,0 +1,16 @@
+console.log("la pyramide Javascript")
+
+let etages = prompt("combien veux tu d'étage ?")
+let diese = "#"
+
+for (let n = 1; n <= etages ; n++)
+{
+    console.log(diese);
+    diese = diese + "#"        
+    
+}
+
+
+
+
+
